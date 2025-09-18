e7d95c5316a4a91400a02de99c85355b49fa8250
murodov1111111-a11y <murodov1111111@gmail.com>
2025-09-18 20:55:49 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+
+
+export default {
+  content: [
+    "./src//*.{html,js}",
+    "./public/index.html",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },  
+  plugins: [],
+};
